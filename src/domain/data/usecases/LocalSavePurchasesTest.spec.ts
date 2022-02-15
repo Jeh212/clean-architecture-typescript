86bb9f6dffc55b874d6301524f7f44212bd449fa
Jean carlos <jeandub1@hotmail.com>
2022-02-14 22:07:25 -0400

    LocalSavePurchases does not delete cache data

--- src/domain/data/usecases/LocalSavePurchasesTest.spec.ts
+++ src/domain/data/usecases/LocalSavePurchasesTest.spec.ts
@@ -1,5 +1,17 @@
 describe("LocalSavePurchasesTest", () => {
-  test("", () => {
-    expect(1).toBe(1);
+  class LocalSavePurchases {
+    constructor(private readonly cacheStore: ICacheStore) {}
+  }
+  interface ICacheStore {}
+
+  class CacheStoreSpy implements ICacheStore {
+    deleteCallsCount = 0;
+  }
+
+  test("It should not delete cache on sut.init", () => {
+    const cacheStore = new CacheStoreSpy();
+    new LocalSavePurchases(cacheStore);
+
+    expect(cacheStore.deleteCallsCount).toBe(0);
   });
 });
