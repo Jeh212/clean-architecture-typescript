e2893ca502a8cf2708a6daa94bee713cd6fca805
Jean carlos <jeandub1@hotmail.com>
2022-02-14 22:16:07 -0400

    ensure LocalSavePrushase delete old cache

--- src/domain/data/usecases/LocalSavePurchasesTest.spec.ts
+++ src/domain/data/usecases/LocalSavePurchasesTest.spec.ts
@@ -1,11 +1,22 @@
 describe("LocalSavePurchasesTest", () => {
   class LocalSavePurchases {
     constructor(private readonly cacheStore: ICacheStore) {}
+
+    async save(): Promise<void> {
+      this.cacheStore.delete();
+    }
   }
-  interface ICacheStore {}
+
+  interface ICacheStore {
+    delete: () => void;
+  }
 
   class CacheStoreSpy implements ICacheStore {
     deleteCallsCount = 0;
+
+    delete(): void {
+      this.deleteCallsCount++;
+    }
   }
 
   test("It should not delete cache on sut.init", () => {
@@ -13,5 +24,12 @@ describe("LocalSavePurchasesTest", () => {
     new LocalSavePurchases(cacheStore);
 
     expect(cacheStore.deleteCallsCount).toBe(0);
+  });
+
+  test("It should delete old cache on sut.save", async () => {
+    const cacheStore = new CacheStoreSpy();
+    const sut = new LocalSavePurchases(cacheStore);
+    await sut.save();
+    expect(cacheStore.deleteCallsCount).toBe(1);
   });
 });
